7a9539a4cf4c4a00553439d32dcce9062c5125a9
hdrafyy <hdrafyy@gmail.com>
2021-11-02 12:56:21 +0800

    Update custom.sh

--- build/Lean_x86_64/custom.sh
+++ build/Lean_x86_64/custom.sh
@@ -151,7 +151,7 @@ EOF
 cat >> .config <<EOF
 CONFIG_PACKAGE_luci-app-oaf=y #应用过滤
 CONFIG_PACKAGE_luci-app-openclash=y #OpenClash客户端
-# CONFIG_PACKAGE_luci-app-serverchan=y #微信推送
+CONFIG_PACKAGE_luci-app-serverchan=y #微信推送
 CONFIG_PACKAGE_luci-app-eqos=y #IP限速
 # CONFIG_PACKAGE_luci-app-control-weburl=y #网址过滤
 # CONFIG_PACKAGE_luci-app-smartdns=y #smartdns服务器
