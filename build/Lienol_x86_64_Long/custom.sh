90363f2e6d411c6c3165f72f9eeb615dd8db69f2
hdrafyy <hdrafyy@gmail.com>
2021-11-06 08:27:05 +0800

    Update custom.sh

--- build/Lienol_x86_64_Long/custom.sh
+++ build/Lienol_x86_64_Long/custom.sh
@@ -115,8 +115,10 @@ EOF
 
 # 网卡驱动:
 cat >> .config <<EOF
-CONFIG_PACKAGE_kmod-r8169=y
 CONFIG_PACKAGE_kmod-phy-realtek=y
+CONFIG_PACKAGE_kmod-r8169=y
+CONFIG_PACKAGE_kmod-rtl8187SE=y
+CONFIG_PACKAGE_luci-app-arpbind=y #单软路由需要添加,双软路由不需要
 EOF
 
 # 编译PVE/KVM、Hyper-V、VMware镜像以及镜像填充
